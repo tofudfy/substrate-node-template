d620c1fcb62d366abad25a7784937d31b4d4f9e0
Fuyang Deng <fd@Fuyangs-MacBook-Pro.local>
2022-01-04 15:22:33 +0800

    Homework of first lesson v1.2 - update Cargo.toml to fix assert_eq_size issue in test.rs

--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -8,7 +8,7 @@ fn create_claim_test() {
 		// Dispatch a claim extrinsic from account 1.
         let proof = vec![0, 1];
 		assert_ok!(PoeModule::create_claim(Origin::signed(1), proof.clone()));
-		
+
         // Read pallet storage and assert an expected result.
 		assert_eq!(
             Proofs::<Test>::get(&proof),
@@ -22,7 +22,7 @@ fn create_claim_exceed_length_limit() {
     new_test_ext().execute_with(|| {
 		// Dispatch a claim extrinsic from account 1 that exceeds 32 bits.
         let proof_exceed = vec![0, 1, 1, 0, 1, 0];
-		
+
         // Execute create claim and assert the ExceedProofLengthLimit error
         assert_noop!(
             PoeModule::create_claim(Origin::signed(1), proof_exceed.clone()),
@@ -37,7 +37,7 @@ fn create_claim_fail_when_claim_exist() {
 		// Dispatch a claim extrinsic from account 1.
         let proof = vec![0, 1];
 		let _ = PoeModule::create_claim(Origin::signed(1), proof.clone());
-	
+
         // Execute create claim and assert the ProofAlreadyClaimed error
 		assert_noop!(
             PoeModule::create_claim(Origin::signed(1), proof.clone()),
@@ -52,7 +52,7 @@ fn revoke_claim_test() {
 		// Dispatch a claim extrinsic from account 1.
         let proof = vec![0, 1];
 		let _ = PoeModule::create_claim(Origin::signed(1), proof.clone());
-	
+
         // Excute revoke claim and assert no errors
         assert_ok!(PoeModule::revoke_claim(Origin::signed(1), proof.clone()));
 
@@ -65,8 +65,8 @@ fn revoke_claim_test() {
 fn revoke_claim_not_exist() {
     new_test_ext().execute_with(|| {
 		// Dispatch a claim extrinsic from account 1.
-        let proof = vec![0, 1];	
-       
+        let proof = vec![0, 1];
+
         // Execute revoke claim and assert the NoSuchProof error
         assert_noop!(
             PoeModule::revoke_claim(Origin::signed(1), proof.clone()),
@@ -79,9 +79,9 @@ fn revoke_claim_not_exist() {
 fn revoke_claim_not_owner() {
     new_test_ext().execute_with(|| {
 		// Dispatch a claim extrinsic from account 1.
-        let proof = vec![0, 1];	
+        let proof = vec![0, 1];
         let _ = PoeModule::create_claim(Origin::signed(1), proof.clone());
-        
+
         // Execute revoke claim and assert the NotProofOwner error
         assert_noop!(
             PoeModule::revoke_claim(Origin::signed(2), proof.clone()),
@@ -100,7 +100,7 @@ fn transfer_claim_test() {
         // Execute transfer claim and assert no errors
         let receiver = 2;
         assert_ok!(PoeModule::transfer_claim(Origin::signed(1), proof.clone(), receiver));
-		
+
         // Read pallet storage and assert the owner is changed to the receiver
         let (owner, _) = Proofs::<Test>::get(&proof);
 		assert_eq!(owner, receiver);
@@ -114,7 +114,7 @@ fn transfer_claim_not_exist() {
         let proof = vec![0, 1];
         let proof2 = vec![0, 2];
 		let _ = PoeModule::create_claim(Origin::signed(1), proof.clone());
-		
+
         let receiver = 2;
         // Transfer the claim ownership and assert the NoSuchProof error.
 		assert_noop!(
@@ -130,7 +130,7 @@ fn transfer_claim_not_owner() {
 		// Dispatch a claim extrinsic from account 1.
         let proof = vec![0, 1];
 		let _ = PoeModule::create_claim(Origin::signed(1), proof.clone());
-		
+
         let receiver = 2;
         // Transger the claim ownership and assert the NotProofOwner error.
 		assert_noop!(
